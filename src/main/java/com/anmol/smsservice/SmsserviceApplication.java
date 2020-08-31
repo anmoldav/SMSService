86f122e9c55e9949183b1129614a346f757cc9f2
anmoldav <anmoldavane@gmail.com>
2020-08-31 19:56:53 +0530

    First Commit

---
+++ src/main/java/com/anmol/smsservice/SmsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.anmol.smsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsserviceApplication.class, args);
+	}
+
+}
